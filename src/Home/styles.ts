456966389acb1ef4d1e1971d64415a97ba40be1f
EzzequielFc <contato.ezequielf@gmail.com>
2023-04-10 08:15:50 -0300

    project completed

---
+++ src/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px 16px 0;
+  background-color: #f1f1f1;
+
+  padding-top: 54px;
+`;
+
+export const Chart = styled.View`
+  width: 100%;
+  align-items: center;
+`;
